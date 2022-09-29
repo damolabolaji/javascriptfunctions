6506a57c434fa0e146b07c81bdefbc32d073dd76
damolabolaji <bolaji.oyindamola@gmail.com>
2022-09-29 17:42:27 +0100

    added snacking function

--- stop-snacking/js/main.js
+++ stop-snacking/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+document.querySelector('#help').addEventListener('click', stopSnacking)
+
+function stopSnacking(){
+    let food = document.querySelector('#foods').value
+    document.querySelector('#stops').innerText += '!' //removes whatever 'stops' already printed
+    for(let i = 1; i <= food; i++){
+        document.querySelector('#stops').innerText += 'stop eating that!'
+    }
+}
