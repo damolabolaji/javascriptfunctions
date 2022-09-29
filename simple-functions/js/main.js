4b52d8317229fdea4867764cff15fcc7734a99e1
damolabolaji <bolaji.oyindamola@gmail.com>
2022-09-29 16:59:42 +0100

    added hard function

--- simple-functions/js/main.js
+++ simple-functions/js/main.js
@@ -14,7 +14,7 @@ divide(100,5/2)
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
 function medium(num1,num2,num3){
-    return (num1 + num2)/num3
+    return (num1 + num2) % num3
 }
 
 alert(medium(20,9,12))
@@ -23,3 +23,18 @@ alert(medium(20,9,12))
 //If the product is greater than 100 add the sum of the last two numbers and console log the value. 
 //If the product is less that 100, subtract the difference of the last two numbers and console log the value. 
 //If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+
+function hard(num1,num2,num3,num4){
+    let result = num1 * num2
+    if(result > 100){
+        console.log(result + num3 + num4)
+    }
+    else if(result < 100){
+        console.log(result - (num3 - num4))
+    }
+    else{
+        alert((num1 * num2 * num3) % num4)
+    }
+}
+
+hard(10,20,30,40)
